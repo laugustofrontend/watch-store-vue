b367ce03a9aaaede40a7b168a9bf7279f3349527
Lucas Augusto <lucas.augusto5061@gmail.com>
2021-02-03 17:22:59 -0300

    add try catch for response on products

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="my-8">
-    <div class="container mx-auto px-6">
+    <div v-if="errorMessage === ''" class="container mx-auto px-6">
       <h3 class="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
       <span class="mt-3 text-sm text-gray-500">200+ Products</span>
       <div
@@ -13,6 +13,7 @@
         />
       </div>
     </div>
+    <h3 v-else class="text-center text-3xl text-red-600">{{ errorMessage }}</h3>
   </main>
 </template>
 
@@ -24,10 +25,16 @@ export default {
   data() {
     return {
       products: [],
+      errorMessage: '',
     }
   },
   async created() {
-    this.products = (await this.$axios.get('/api/products')).data.products
+    try {
+      this.products = (await this.$axios.get('/api/products')).data.products
+    } catch (e) {
+      console.error('server error => ', e)
+      this.errorMessage = 'Problemas ao carregar a lista'
+    }
   },
 }
 </script>
