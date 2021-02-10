1587d7d75203dc34fd4423a1ebf902a4682f29f6
Lucas Augusto <lucas.augusto5061@gmail.com>
2021-02-10 17:04:53 -0300

    finish test for product card, add method addToCart

--- components/Product/card.unit.spec.js
+++ components/Product/card.unit.spec.js
@@ -4,6 +4,23 @@ import { makeServer } from '@/miragejs/server'
 
 describe('Product Card - unit', () => {
   let server
+  const mountProductCard = () => {
+    const product = server.create('product', {
+      title: 'Relógio bonito',
+      price: '22.00',
+      image:
+        'https://images.unsplash.com/photo-1495856458515-0637185db551?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+    })
+
+    return {
+      wrapper: mount(ProductCard, {
+        propsData: {
+          product,
+        },
+      }),
+      product,
+    }
+  }
 
   beforeEach(() => {
     server = makeServer({ environment: 'test' })
@@ -13,14 +30,7 @@ describe('Product Card - unit', () => {
   })
 
   it('should mount the component', () => {
-    const wrapper = mount(ProductCard, {
-      propsData: {
-        product: server.create('product', {
-          title: 'Relógio bonito',
-          price: '22.00',
-        }),
-      },
-    })
+    const { wrapper } = mountProductCard()
 
     expect(wrapper.vm).toBeDefined()
     expect(wrapper.text()).toContain('Relógio bonito')
@@ -28,17 +38,18 @@ describe('Product Card - unit', () => {
   })
 
   it('should match snapshot', () => {
-    const wrapper = mount(ProductCard, {
-      propsData: {
-        product: server.create('product', {
-          title: 'Relógio bonito',
-          price: '22.00',
-          image:
-            'https://images.unsplash.com/photo-1495856458515-0637185db551?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-        }),
-      },
-    })
+    const { wrapper } = mountProductCard()
 
     expect(wrapper.element).toMatchSnapshot()
+  })
+
+  it('should emit the event addToCard with product object when button gets clicked', async () => {
+    const { wrapper, product } = mountProductCard()
+
+    await wrapper.find('button').trigger('click')
+
+    expect(wrapper.emitted().addToCart).toBeTruthy()
+    expect(wrapper.emitted().addToCart.length).toBe(1)
+    expect(wrapper.emitted().addToCart[0]).toEqual([{ product }])
   })
 })
