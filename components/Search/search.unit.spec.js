9a4605a913ecd187f2169756bf90b6ee6f65acc9
Lucas Augusto <lucas.augusto5061@gmail.com>
2021-02-10 19:37:44 -0300

    finish test for search

--- components/Search/search.unit.spec.js
+++ components/Search/search.unit.spec.js
@@ -19,4 +19,17 @@ describe('Search - unit', () => {
     expect(wrapper.emitted().doSearch.length).toBe(1)
     expect(wrapper.emitted().doSearch[0]).toEqual([{ term }])
   })
+
+  it('should emit search event when search search input is cleared', async () => {
+    const wrapper = mount(Search)
+    const term = 'termo para buscar'
+    const input = wrapper.find('input[type="search"]')
+
+    await input.setValue(term)
+    await input.setValue('')
+
+    expect(wrapper.emitted().doSearch).toBeTruthy()
+    expect(wrapper.emitted().doSearch.length).toBe(1)
+    expect(wrapper.emitted().doSearch[0]).toEqual([{ term: '' }])
+  })
 })
