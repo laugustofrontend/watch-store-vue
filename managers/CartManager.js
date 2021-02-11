f115558328e5cdaba2d1d248a7c0f6380afc0efb
Lucas Augusto <lucas.augusto5061@gmail.com>
2021-02-11 16:49:40 -0300

    create and tested cart manager

---
+++ managers/CartManager.js
@@ -0,0 +1,67 @@
+import Vue from 'vue'
+
+const initialState = {
+  open: false,
+  items: [],
+}
+
+export class CartManager {
+  state
+
+  constructor() {
+    this.state = Vue.observable(initialState)
+  }
+
+  getState() {
+    return this.state
+  }
+
+  open() {
+    this.state.open = true
+
+    return this.getState()
+  }
+
+  close() {
+    this.state.open = false
+
+    return this.getState()
+  }
+
+  productIsTheCart(product) {
+    return !!this.state.items.find(({ id }) => id === product.id)
+  }
+
+  hasProduct() {
+    return this.state.items.length > 0
+  }
+
+  addProduct(product) {
+    if (!this.productIsTheCart(product)) {
+      this.state.items.push(product)
+    }
+
+    return this.getState()
+  }
+
+  removeProduct(productId) {
+    this.state.items = [
+      ...this.state.items.filter((product) => product.id !== productId),
+    ]
+
+    return this.getState()
+  }
+
+  clearProducts() {
+    this.state.items = []
+
+    return this.getState()
+  }
+
+  clearCart() {
+    this.clearProducts()
+    this.close()
+
+    return this.getState()
+  }
+}
