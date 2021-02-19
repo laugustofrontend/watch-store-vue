ba4553b52a3a1a762d4f30f7cddf28a7f555762b
Lucas Augusto <lucas.augusto5061@gmail.com>
2021-02-19 11:35:14 -0300

    create reference for cy.get

--- cypress/e2e/Store.spec.js
+++ cypress/e2e/Store.spec.js
@@ -5,6 +5,7 @@ import { makeServer } from '../../miragejs/server'
 
 context('Store', () => {
   let server
+  const g = cy.get
 
   beforeEach(() => {
     server = makeServer({ environment: 'test' })
@@ -17,15 +18,15 @@ context('Store', () => {
   it('should display the store', () => {
     cy.visit('/')
 
-    cy.get('body').contains('Brand')
-    cy.get('body').contains('Wrist Watch')
+    g('body').contains('Brand')
+    g('body').contains('Wrist Watch')
   })
 
   context('Store > Search for products', () => {
     it('should type in the search field', () => {
       cy.visit('/')
 
-      cy.get('input[type="search"')
+      g('input[type="search"')
         .type('Some text here')
         .should('have.value', 'Some text here')
     })
@@ -37,61 +38,61 @@ context('Store', () => {
       server.createList('product', 10)
 
       cy.visit('/')
-      cy.get('input[type="search"').type('Relógio bonito')
-      cy.get('[data-testid="search-form"]').submit()
-      cy.get('[data-testid="product-card"]').should('have.length', 1)
+      g('input[type="search"').type('Relógio bonito')
+      g('[data-testid="search-form"]').submit()
+      g('[data-testid="product-card"]').should('have.length', 1)
     })
 
     it('should type in the search field', () => {
       server.createList('product', 10)
 
       cy.visit('/')
-      cy.get('input[type="search"').type('Relógio bonito')
-      cy.get('[data-testid="search-form"]').submit()
-      cy.get('[data-testid="product-card"]').should('have.length', 0)
-      cy.get('body').contains('0 Products')
+      g('input[type="search"').type('Relógio bonito')
+      g('[data-testid="search-form"]').submit()
+      g('[data-testid="product-card"]').should('have.length', 0)
+      g('body').contains('0 Products')
     })
   })
 
   context('Store > Product List', () => {
     it('should display "0 Products" when no product is returned', () => {
       cy.visit('/')
-      cy.get('[data-testid="product-card"]').should('have.length', 0)
-      cy.get('body').contains('0 Product')
+      g('[data-testid="product-card"]').should('have.length', 0)
+      g('body').contains('0 Product')
     })
 
     it('should display "1 Products" when 1 product is returned', () => {
       server.create('product')
 
       cy.visit('/')
-      cy.get('[data-testid="product-card"]').should('have.length', 1)
-      cy.get('body').contains('1 Product')
+      g('[data-testid="product-card"]').should('have.length', 1)
+      g('body').contains('1 Product')
     })
 
     it('should display "10 Products" when 10 product is returned', () => {
       server.createList('product', 10)
 
       cy.visit('/')
-      cy.get('[data-testid="product-card"]').should('have.length', 10)
-      cy.get('body').contains('10 Products')
+      g('[data-testid="product-card"]').should('have.length', 10)
+      g('body').contains('10 Products')
     })
   })
 
   context('Store > Shopping Cart', () => {
     it('should not display shopping cart when page first loads', () => {
       cy.visit('/')
-      cy.get('[data-testid="shopping-cart"]').should('have.class', 'hidden')
+      g('[data-testid="shopping-cart"]').should('have.class', 'hidden')
     })
 
     it('should toggle shopping cart visibility when button is clicked', () => {
       cy.visit('/')
-      cy.get('[data-testid="toggle-button"]').as('toggleButton')
+      g('[data-testid="toggle-button"]').as('toggleButton')
 
-      cy.get('@toggleButton').click()
-      cy.get('[data-testid="shopping-cart"]').should('have.not', 'hidden')
+      g('@toggleButton').click()
+      g('[data-testid="shopping-cart"]').should('have.not', 'hidden')
 
-      cy.get('@toggleButton').click({ force: true })
-      cy.get('[data-testid="shopping-cart"]').should('have.class', 'hidden')
+      g('@toggleButton').click({ force: true })
+      g('[data-testid="shopping-cart"]').should('have.class', 'hidden')
     })
   })
 })
