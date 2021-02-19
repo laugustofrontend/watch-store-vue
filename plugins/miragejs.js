c2451eef91eccbf3e600e7dcc8c4aa4792e5b3ee
Lucas Augusto <lucas.augusto5061@gmail.com>
2021-02-19 10:43:47 -0300

    add Cypress in miragejs and change mod nuxt for spa

--- plugins/miragejs.js
+++ plugins/miragejs.js
@@ -1,3 +1,26 @@
+import { createServer, Response } from 'miragejs'
+
 if (process.env.NODE_ENV === 'development') {
   require('@/miragejs/server').makeServer()
+}
+
+if (window.Cypress) {
+  const otherDomains = []
+  const methods = ['get', 'post', 'patch', 'post', 'delete']
+
+  createServer({
+    routes() {
+      for (const domain of ['/', ...otherDomains]) {
+        for (const method of methods) {
+          this[method](`${domain}*`, async (schema, request) => {
+            const [status, headers, body] = await window.handleFromCypress(
+              request
+            )
+
+            return new Response(status, headers, body)
+          })
+        }
+      }
+    },
+  })
 }
