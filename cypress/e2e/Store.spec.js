a7d1c4744d4465e5acc1dd038e800427f9ecd233
Lucas Augusto <lucas.augusto5061@gmail.com>
2021-02-19 11:22:51 -0300

    create test for product list

--- cypress/e2e/Store.spec.js
+++ cypress/e2e/Store.spec.js
@@ -15,7 +15,7 @@ context('Store', () => {
   })
 
   it('should display the store', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('/')
 
     cy.get('body').contains('Brand')
     cy.get('body').contains('Wrist Watch')
@@ -23,7 +23,7 @@ context('Store', () => {
 
   context('Store > Search for products', () => {
     it('should type in the search field', () => {
-      cy.visit('http://localhost:3000')
+      cy.visit('/')
 
       cy.get('input[type="search"')
         .type('Some text here')
@@ -36,7 +36,7 @@ context('Store', () => {
       })
       server.createList('product', 10)
 
-      cy.visit('http://localhost:3000')
+      cy.visit('/')
       cy.get('input[type="search"').type('Relógio bonito')
       cy.get('[data-testid="search-form"]').submit()
       cy.get('[data-testid="product-card"]').should('have.length', 1)
@@ -45,11 +45,35 @@ context('Store', () => {
     it('should type in the search field', () => {
       server.createList('product', 10)
 
-      cy.visit('http://localhost:3000')
+      cy.visit('/')
       cy.get('input[type="search"').type('Relógio bonito')
       cy.get('[data-testid="search-form"]').submit()
       cy.get('[data-testid="product-card"]').should('have.length', 0)
       cy.get('body').contains('0 Products')
+    })
+  })
+
+  context('Store > Product List', () => {
+    it('should display "0 Products" when no product is returned', () => {
+      cy.visit('/')
+      cy.get('[data-testid="product-card"]').should('have.length', 0)
+      cy.get('body').contains('0 Product')
+    })
+
+    it('should display "1 Products" when 1 product is returned', () => {
+      server.create('product')
+
+      cy.visit('/')
+      cy.get('[data-testid="product-card"]').should('have.length', 1)
+      cy.get('body').contains('1 Product')
+    })
+
+    it('should display "10 Products" when 10 product is returned', () => {
+      server.createList('product', 10)
+
+      cy.visit('/')
+      cy.get('[data-testid="product-card"]').should('have.length', 10)
+      cy.get('body').contains('10 Products')
     })
   })
 })
